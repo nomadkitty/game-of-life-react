6abf8f91664d192cb44b47d3becf41f05c9fc457
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-23 18:26:13 -0400

    Set up files

---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Grid from "./Grid";
+import Toolbox from "./Toolbox";
+
+function Dashboard(props) {
+  return (
+    <>
+      <h1>The Game of Life</h1>
+      {/* <Grid /> */}
+      {/* <Toolbox /> */}
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/Grid.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Grid(props) {
+  const [grid, setGride] = useState();
+
+  return <div>{grid}</div>;
+}
