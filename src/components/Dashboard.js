ee6ce0f3e740e3739234e9cb7fd51427fdae8c26
Jojo Zhang <jojozhang0327@gmail.com>
2020-06-23 20:18:41 -0400

    Create empty grid

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ function Dashboard(props) {
   return (
     <>
       <h1>The Game of Life</h1>
-      {/* <Grid /> */}
+      <Grid />
       {/* <Toolbox /> */}
     </>
   );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,30 @@
 import React, { useState } from "react";
 
-function Grid(props) {
-  const [grid, setGride] = useState();
+const numRows = 50;
+const numCols = 50;
 
-  return <div>{grid}</div>;
+const createEmptyGrid = (numRows, numCols) => {
+  const grid = [];
+  for (let i = 0; i < numRows; i++) {
+    const cols = [];
+    for (let j = 0; j < numCols; j++) {
+      cols.push(0);
+    }
+    grid.push(cols);
+  }
+  return grid;
+};
+
+function Grid(props) {
+  const [grid, setGride] = useState(createEmptyGrid(numRows, numCols));
+  console.log(grid);
+  return (
+    <div>
+      {grid.map((row, rowIdx) => {
+        row.map();
+      })}
+    </div>
+  );
 }
+
+export default Grid;
